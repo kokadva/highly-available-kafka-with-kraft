f08d68b22825915ea8b21abda40aabb6a311e824
konstantinedvalishvili <cdval13@freeuni.edu.ge>
2023-08-07 17:50:23 +0400

    add kafka properties

--- docker/config_setup.sh
+++ docker/config_setup.sh
@@ -4,6 +4,20 @@
 KAFKA_DIR="/app/kafka"
 SERVER_PROPERTIES_DIR=$KAFKA_DIR/server.properties
 KAFKA_BIN_DIR="${KAFKA_DIR}/bin"
+PROCESS_ROLES="${PROCESS_ROLES:-broker,controller}"
+NUM_NETWORK_THREADS="${NUM_NETWORK_THREADS:-2}"
+NUM_IO_THREADS="${NUM_IO_THREADS:-4}"
+SOCKET_SEND_BUFFER_BYTES="${SOCKET_SEND_BUFFER_BYTES:-102400}"
+SOCKET_RECEIVE_BUFFER_BYTES="${SOCKET_RECEIVE_BUFFER_BYTES:-102400}"
+SOCKET_REQUEST_MAX_BYTES="${SOCKET_REQUEST_MAX_BYTES:-104857600}"
+NUM_PARTITIONS="${NUM_PARTITIONS:-1}"
+NUM_RECOVERY_THREADS_PER_DATA_DIR="${NUM_RECOVERY_THREADS_PER_DATA_DIR:-1}"
+OFFSETS_TOPIC_REPLICATION_FACTOR="${OFFSETS_TOPIC_REPLICATION_FACTOR:-1}"
+TRANSACTION_STATE_LOG_REPLICATION_FACTOR="${TRANSACTION_STATE_LOG_REPLICATION_FACTOR:-1}"
+TRANSACTION_STATE_LOG_MIN_ISR="${TRANSACTION_STATE_LOG_MIN_ISR:-1}"
+LOG_RETENTION_HOURS="${LOG_RETENTION_HOURS:-168}"
+LOG_SEGMENT_BYTES="${LOG_SEGMENT_BYTES:-1073741824}"
+LOG_RETENTION_CHECK_INTERVAL_MS="${LOG_RETENTION_CHECK_INTERVAL_MS:-300000}"
 STORAGE_DIR="${STORAGE_DIR:-/mtn}"
 LOG_DIRS=$STORAGE_DIR/$NODE_ID
 CLUSTER_ID="${CLUSTER_ID:-oh-sxaDRTcyAr6pFRbXyzA}"
@@ -27,6 +41,20 @@ CONTROLLER_QUORUM_VOTERS=${CONTROLLER_QUORUM_VOTERS::-1}
 update_kafka_configuration() {
     sed -i \
     -e "s+^controller.quorum.voters=.*+controller.quorum.voters=$CONTROLLER_QUORUM_VOTERS+" \
+    -e "s+^process.roles=.*+process.roles=$PROCESS_ROLES+" \
+    -e "s+^num.network.threads=.*+num.network.threads=$NUM_NETWORK_THREADS+" \
+    -e "s+^num.io.threads=.*+num.io.threads=$NUM_IO_THREADS+" \
+    -e "s+^socket.send.buffer.bytes=.*+socket.send.buffer.bytes=$SOCKET_SEND_BUFFER_BYTES+" \
+    -e "s+^socket.receive.buffer.bytes=.*+socket.receive.buffer.bytes=$SOCKET_RECEIVE_BUFFER_BYTES+" \
+    -e "s+^socket.request.max.bytes=.*+socket.request.max.bytes=$SOCKET_REQUEST_MAX_BYTES+" \
+    -e "s+^num.partitions=.*+num.partitions=$NUM_PARTITIONS+" \
+    -e "s+^num.recovery.threads.per.data.dir=.*+num.recovery.threads.per.data.dir=$NUM_RECOVERY_THREADS_PER_DATA_DIR+" \
+    -e "s+^offsets.topic.replication.factor=.*+offsets.topic.replication.factor=$OFFSETS_TOPIC_REPLICATION_FACTOR+" \
+    -e "s+^transaction.state.log.replication.factor=.*+transaction.state.log.replication.factor=$TRANSACTION_STATE_LOG_REPLICATION_FACTOR+" \
+    -e "s+^transaction.state.log.min.isr=.*+transaction.state.log.min.isr=$TRANSACTION_STATE_LOG_MIN_ISR+" \
+    -e "s+^log.retention.hours=.*+log.retention.hours=$LOG_RETENTION_HOURS+" \
+    -e "s+^log.segment.bytes=.*+log.segment.bytes=$LOG_SEGMENT_BYTES+" \
+    -e "s+^log.retention.check.interval.ms=.*+log.retention.check.interval.ms=$LOG_RETENTION_CHECK_INTERVAL_MS+" \
     -e "s+^node.id=.*+node.id=$NODE_ID+" \
     -e "s+^log.dirs=.*+log.dirs=$LOG_DIRS+" \
     -e "s+^listeners=.*+listeners=$LISTENERS+" \
