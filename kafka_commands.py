1ebd6add4b23c0cba2eaf2f67db77a36bef58696
konstantinedvalishvili <cdval13@freeuni.edu.ge>
2023-08-19 00:35:42 +0400

    fix config

--- kafka_commands.py
+++ kafka_commands.py
@@ -1,5 +1,5 @@
-bootstrap_servers = "kafka-0.kafka-svc.default.svc.cluster.local:19093,kafka-1.kafka-svc.default.svc.cluster.local:19093,kafka-2.kafka-svc.default.svc.cluster.local:19093"
-topic = "test"
+bootstrap_servers = "kafka-0.kafka-svc.kafka.svc.cluster.local:19093,kafka-1.kafka-svc.kafka.svc.cluster.local:19093,kafka-2.kafka-svc.kafka.svc.cluster.local:19093"
+topic = "order_order__init"
 replication_factor = 3
 partitions = 1
 consumers_group = 'consumers'
@@ -8,7 +8,7 @@ kafka_commands = {
     "Describe topic": f"./kafka-topics.sh --describe --topic {topic} --bootstrap-server {bootstrap_servers}",
     "Create topic": f"./kafka-topics.sh --bootstrap-server {bootstrap_servers} --create --replication-factor {replication_factor} --partitions {partitions} --topic {topic}",
     "List topics": f"./kafka-topics.sh --bootstrap-server {bootstrap_servers} --list",
-    "Delete topic": f"kafka-topics.sh --bootstrap-server {bootstrap_servers} --delete --topic {topic}",
+    "Delete topic": f"./kafka-topics.sh --bootstrap-server {bootstrap_servers} --delete --topic {topic}",
     "Producer": f"./kafka-console-producer.sh --topic {topic} --broker-list {bootstrap_servers}",
     "Consumer": f"./kafka-console-consumer.sh --topic {topic} --bootstrap-server {bootstrap_servers} --group {consumers_group} --from-beginning",
     "Broker API": f"./kafka-broker-api-versions.sh --bootstrap-server {bootstrap_servers}"
